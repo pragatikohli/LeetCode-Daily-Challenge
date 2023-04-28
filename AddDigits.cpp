0bb90698bdd647ba1e2ce23401ba756f46edcb53
pragatikohli <pragatikohli12@gmail.com>
2023-04-28 14:10:49 +0530

    Solution Added

---
+++ AddDigits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        int sum = 0;
+        while (num)
+        {
+            sum += (num % 10);
+            num = num / 10;
+        }
+        if (sum < 10)
+            return sum;
+        return addDigits(sum);
+    }
+};
